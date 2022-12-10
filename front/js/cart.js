9c377d1ca95eb28c9a767dc02ad2f844aec44cfe
jssdestroyerman <114258377+jssdestroyerman@users.noreply.github.com>
2022-12-10 15:05:29 +0100

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -146,7 +146,11 @@ function totalQuantity(quantity) {
     cartTotalQuantity.innerText = finalQuantity
 }
 
-/****************** FORM *******************/
+
+
+
+// FORM ------------------------------>
+
 
 const emailRegex = /^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/
 const letterRegex = /([A-Z]+[A-Z ])\w+/gi
